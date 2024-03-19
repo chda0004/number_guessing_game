a5cd1b6f6a775bb3a178feb012026d84388fa431
chda0004 d <christian@die-danys.de>
2024-03-19 13:52:42 +0000

    feat: Main Menue else

--- number_guess.sh
+++ number_guess.sh
@@ -13,10 +13,19 @@ then
   #user not found. Create him
   RETURN_CREATE_USER=$($PSQL "INSERT INTO users(username) VALUES('$USER_NAME')")
   echo Welcome, $USER_NAME! It looks like this is your first time here.
+  RETURN_USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USER_NAME'")
 else
 #user found. go to the play
-echo else
+
+RETURN_PLAYED_GAMES=$($PSQL "SELECT games_played FROM rank WHERE user_id='$RETURN_USER_ID'")
+RETURN_HIGHSCORE=$($PSQL "SELECT best_game FROM rank WHERE user_id='$RETURN_USER_ID'")
+echo Welcome back, $USER_NAME! You have played $RETURN_PLAYED_GAMES games, and your best game took $RETURN_HIGHSCORE guesses.
 fi
+}
+
+GAME(){
+
+echo game
 }
 
 MAIN_MENU
