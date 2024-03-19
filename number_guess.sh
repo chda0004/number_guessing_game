7c91cb352aff625ad7bae9824a71d5331b2207b8
chda0004 d <christian@die-danys.de>
2024-03-19 13:46:10 +0000

    feat:Create MAIN MENUE

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+MAIN_MENU(){
+
+
+echo Enter your username:
+read USER_NAME
+RETURN_USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USER_NAME'")
+
+if [[ -z $RETURN_USER_ID ]]
+then
+  #user not found. Create him
+  RETURN_CREATE_USER=$($PSQL "INSERT INTO users(username) VALUES('$USER_NAME')")
+  echo Welcome, $USER_NAME! It looks like this is your first time here.
+else
+#user found. go to the play
+echo else
+fi
+}
+
+MAIN_MENU
